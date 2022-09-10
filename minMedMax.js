bbabe5be1702c9a8e70114e094b0f6f0e419a230
isPetch <88103945+isPetch@users.noreply.github.com>
2022-09-10 14:48:40 +0700

    Update minMedMax.js

--- minMedMax.js
+++ minMedMax.js
@@ -2,5 +2,32 @@ const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
   //code here
+    if (n1 < n2  < n3) {
+    return Object = {min:n1,mid:n2,max:n3}
+  }else if(n1 < n3 < n2){
+    return Object = {min:n1,mid:n3,max:n2}
+  }else if(n3 < n1 < n2){
+    return Object= {min:n3,mid:n1,max:n2}
+  }else if(n3 < n2 < n1){
+    return Object = {min:n3,mid:n2,max:n1}
+  }else if(n2 < n1 < n3){
+    return Object = {min:n2,mid:n1,max:n3}
+  }else if(n2 < n3 < n1){
+    return Object = {min:n2,mid:n3,max:n1}
+  }else if(n1 == n2 < n3){
+     return Object = {min:n1,mid:n2,max:n3}
+   }else if(n1 == n2  > n3){
+     return Object = {min:n3,mid:n1,max:n2}
+   }else if(n1 == n3  < n2){
+     return Object = {min:n1,mid:n3,max:n2}
+   }else if(n1 == n3  > n2){
+     return Object = {min:n1,mid:n2,max:n3}
+   }else if(n2 == n3  < n1){
+     return Object = {min:n2,mid:n3,max:n1}
+   }else if(n2 == n3  > n1){
+     return Object = {min:n1,mid:n2,max:n3}   
+   }else{
+     return Object = {min:n1,mid:n2,max:n3}
+   }
 }
 module.exports = minMedMax
